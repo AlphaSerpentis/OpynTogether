63b380e9a91027c9e2dcfb4d2289300900cfd13e
AlphaSerpentis <37189157+AlphaSerpentis@users.noreply.github.com>
2021-03-07 22:44:41 -0600

    added deposit/withdraw functions

--- contracts/OpynTogether.sol
+++ contracts/OpynTogether.sol
@@ -20,7 +20,8 @@ contract OpynTogether {
     struct Position {
         address liquidityPool;
         uint256 timeOfExpiration;
-        Participant[] participants;
+        mapping(address => Participant) participants;
+        bool open;
     }
 
     /**
@@ -49,13 +50,17 @@ contract OpynTogether {
         _onlyAdmin();
         _;
     }
+    modifier onlyParticipant(address _oToken) {
+        _onlyParticipant(_oToken);
+        _;
+    }
 
     function openNewPosition(
         address _oToken,
         address _liquidityPool
     ) external {
         require(
-            _oToken != address(0) && _liquidityPool != address(0),
+            _oToken != address(0),
             "Zero address"
         );
 
@@ -67,7 +72,46 @@ contract OpynTogether {
             position.liquidityPool = _liquidityPool;
         }
 
+        //position.timeOfExpiration = make a call to the oToken and grab its expiration value
+        position.open = true;
         emit PositionOpened(_oToken);
+    }
+    function closePosition(
+        address _oToken
+    )
+        external onlyParticipant(_oToken)
+    {
+        // Require statement for oToken to check if zero address is not needed due to modifier
+        Position storage position = positions[_oToken];
+        require(
+            position.timeOfExpiration >= block.timestamp,
+            "Cannot close position"
+        );
+        position.open = false;
+
+        emit PositionClosed(_oToken);
+    }
+    function depositIntoPosition(
+        address _oToken,
+        uint256 _oTokenDeposit
+    )
+        external onlyParticipant(_oToken)
+    {
+        Position storage position = positions[_oToken];
+
+        // ERC20 transferFrom from msg.sender to liquidity pool
+        position.participants[msg.sender].liquidityAmount += _oTokenDeposit;
+    }
+    function withdrawInPosition(
+        address _oToken,
+        uint256 _oTokenWithdraw
+    )
+        external onlyParticipant(_oToken)
+    {
+        Position storage position = positions[_oToken];
+        
+        // Make a call to withdraw _oTokenWithdraw...
+        position.participants[msg.sender].liquidityAmount -= _oTokenWithdraw;
     }
     function changeAllowedTimeBeforeExpiration(
         uint256 _newTime
@@ -82,6 +126,12 @@ contract OpynTogether {
             "Zero address"
         );
         admin = _admin;
+    }
+    function _onlyParticipant(address _oToken) internal view {
+        require(
+            positions[_oToken].participants[msg.sender].user != address(0),
+            "Unauthorized (participant)"
+        );
     }
     function _onlyAdmin() internal view {
         require(
